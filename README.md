c97f9f8c6a1095b642978dcf90fafb5ad7618f40
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-10 15:32:35 -0300

    change Version2

---
+++ README.md
@@ -0,0 +1,3 @@
+## todo list v2
+
+![](./public/imgs/preview.png)
--- app.js
+++ app.js
@@ -1,34 +1,124 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose")
+const _ = require("lodash")
 
 const app = express()
 
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.static("public"))
+// password removed for secure reasons
+mongoose.connect("mongodb+srv://rgnh55:@rafael-cluster.xsxmtkv.mongodb.net/todolistDB")
 
-const items = ["Buy Yu-Gi-Oh! cards", "Open the deck", "Play with friends"]
-const games = ["League of Legends", "Genshin Impact", "Dbz Budokai T 3"]
+const itemSchema = new mongoose.Schema({
+    name: String,
+    type: String
+})
 
-app.get("/", function(req, res) {
-    const day = date.getDate()
+const Item = mongoose.model("Item", itemSchema)
 
-    res.render("list", {
-        listTitle: day,
-        items: items
+const obelisk = new Item({
+    name: "Obelisk The Tormentor",
+    type: "Divine Beast"
+})
+
+const dragon_ra = new Item({
+    name: "The Winged Dragon of Ra",
+    type: "Divine Beast"
+})
+
+const blue_eyes_dragon = new Item({
+    name: "Blue-Eyes White Dragon",
+    type: "Dragon"
+})
+
+const defaultItems = [obelisk, dragon_ra, blue_eyes_dragon]
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema)
+
+app.get("/", function (req, res) {
+    Item.find({}, (err, items) => {
+        if (items.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (!err) {
+                    console.log("Root route populated")
+                }
+            })
+            res.redirect("/")
+        } else {
+            res.render("list", {
+                listTitle: "Today",
+                items: items
+            })
+        }
+    })
+})
+
+app.get("/:foo", (req, res) => {
+    const foo = _.capitalize(req.params.foo)
+
+    List.findOne({ name: foo }, (err, result) => {
+        if (!result) {
+            const list = new List({
+                name: foo,
+                items: defaultItems
+            })
+            list.save()
+            res.redirect("/" + foo)
+        } else {
+            res.render("list", { listTitle: result.name, items: result.items })
+        }
     })
 })
 
 app.post("/", (req, res) => {
-    const newItem = req.body.newItem
+    const itemName = req.body.newItem
+    const listName = req.body.list
+    const item = new Item({
+        name: itemName
+    })
 
-    if (req.body.button === "Games") {
-        games.push(newItem)
-        res.redirect("/games")
-    } else {
-        items.push(newItem)
+    if (listName === "Today") {
+        item.save()
         res.redirect("/")
+    } else {
+        List.findOne({ name: listName }, (err, foundList) => {
+            foundList.items.push(item)
+            foundList.save()
+            res.redirect("/" + listName)
+        })
+    }
+})
+
+app.post("/delete", (req, res) => {
+    const itemId = req.body.deleteItem
+    const listName = req.body.listName
+
+    if (listName === "Today") {
+        Item.deleteOne({ _id: itemId }, (err) => {
+            if (!err) {
+                console.log("Deleted document from root route")
+            }
+        })
+        res.redirect("/")
+    } else {
+        List.findOneAndUpdate(
+            { name: listName },
+            { $pull: { items: { _id: itemId } } },
+            (err, foundList) => {
+                if (!err) {
+                    res.redirect("/" + listName)
+                }
+            }
+        )
     }
 })
 
@@ -43,6 +133,6 @@ app.get("/about", (req, res) => {
     res.render("about")
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server started on port 3000")
 })
